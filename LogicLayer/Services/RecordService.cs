465db3476e509bbe646f2023fe5fa029dcb16e86
PedenkoIllia <pedenkoia@gmail.com>
2020-08-22 18:30:16 +0300

    Add Dispose method. Set IUnitOfWork instread IRepository

---
+++ LogicLayer/Services/RecordService.cs
@@ -0,0 +1,55 @@
+﻿using AutoMapper;
+using DataAccessLayer.Entities;
+using DataAccessLayer.Interfaces;
+using LogicLayer.DataTransferObjects;
+using LogicLayer.Interfaces;
+using System.Collections.Generic;
+
+namespace LogicLayer.Services
+{
+    public class RecordService : IRecordService
+    {
+        IUnitOfWork Database { get; set; }
+
+        public RecordService(IUnitOfWork db)
+        {
+            Database = db;
+        }
+
+        public void AddRecord(RecordDTO rec)
+        {
+            Record record = CreateFromDTO(rec);
+
+            Database.Records.Create(record);
+            Database.Save();
+        }
+
+        public void ChangeRecord(RecordDTO rec)
+        {
+            Record record = CreateFromDTO(rec);
+            Database.Records.Update(record);
+        }
+
+        public IEnumerable<RecordDTO> GetRecords()
+        {
+            var mapper = new MapperConfiguration(cfg => cfg.CreateMap<Record, RecordDTO>()).CreateMapper();
+            return mapper.Map<IEnumerable<Record>, List<RecordDTO>>(Database.Records.GetAll());
+        }
+
+        private Record CreateFromDTO(RecordDTO rec) 
+        {
+            Record record = new Record()
+            {
+                Code = rec.Code,
+                Name = rec.Name
+            };
+
+            return record;
+        }
+
+        public void Dispose()
+        {
+            Database.Dispose();
+        }
+    }
+}
