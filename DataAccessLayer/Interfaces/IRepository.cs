67b0334e24469797414fc930f1001151a02838bf
PedenkoIllia <pedenkoia@gmail.com>
2020-08-22 18:24:33 +0300

    Use UnitOfWork pattern. Move Save function from IRepository to IUnitOfWork

--- DataAccessLayer/Interfaces/IRepository.cs
+++ DataAccessLayer/Interfaces/IRepository.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 
 namespace DataAccessLayer.Interfaces
 {
-    public interface IRepository<T> where T : class
+    public interface IRepository<T> where T : class 
     {
         IEnumerable<T> GetAll();
         T Get(int id);
