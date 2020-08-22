8a1ed2ac7cc6a786e2a4cce9cde52dc35b200ca1
PedenkoIllia <pedenkoia@gmail.com>
2020-08-22 15:26:34 +0300

    Add db context and configuration for record

---
+++ DataAccessLayer/EntityFramework/RecordConfiguration.cs
@@ -0,0 +1,20 @@
+﻿using DataAccessLayer.Entities;
+using System.Data.Entity.ModelConfiguration;
+
+namespace DataAccessLayer.EntityFramework
+{
+    class RecordConfiguration : EntityTypeConfiguration<Record>
+    {
+        public RecordConfiguration() {
+            ToTable("Records").HasKey(r => r.Id);
+            Property(r => r.Code)
+                .HasMaxLength(3)
+                .HasColumnType("varchart")
+                .HasColumnName("code");
+
+            Property(r => r.Name).
+                HasColumnType("varchart").
+                HasColumnName("value");
+        }
+    }
+}
---
+++ DataAccessLayer/EntityFramework/RecordContext.cs
@@ -0,0 +1,20 @@
+﻿using DataAccessLayer.Entities;
+using System.Data.Entity;
+
+namespace DataAccessLayer.EntityFramework
+{
+    class RecordContext : DbContext
+    {
+        public DbSet<Record> Records { get; set; }
+
+        public RecordContext(string connectionString)
+                    : base(connectionString)
+        {
+        }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Configurations.Add(new RecordConfiguration());
+        }
+    }
+}
