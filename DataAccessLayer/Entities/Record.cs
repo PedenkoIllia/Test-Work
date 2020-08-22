a09f4337c4e9a0f79004f721d447d106628f08da
PedenkoIllia <pedenkoia@gmail.com>
2020-08-22 15:24:52 +0300

    Add Record entity

---
+++ DataAccessLayer/Entities/Record.cs
@@ -0,0 +1,9 @@
+﻿namespace DataAccessLayer.Entities
+{
+    class Record
+    {
+        public int Id { get; set; }
+        public string Code { get; set; }
+        public string Name { get; set; }
+    }
+}
