dc9e14000b13e93a927c3aceb74439f04d7e1817
PedenkoIllia <pedenkoia@gmail.com>
2020-08-23 16:42:29 +0300

    Create model validator

---
+++ WEB/Validators/RecordModelValidator.cs
@@ -0,0 +1,15 @@
+﻿using FluentValidation;
+using WEB.Models;
+
+namespace WEB.Validators
+{
+    public class RecordValidator : AbstractValidator<RecordViewModel>
+    {
+        public RecordValidator() 
+        {
+            RuleFor(rec => rec.Id).NotNull();
+            RuleFor(rec => rec.Code).Matches("^\\d{3}$");
+            RuleFor(rec => rec.Name).NotEmpty();
+        }
+    }
+}
