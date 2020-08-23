27fdf87eb994bd937b5bce8ffc9d1c9d05533418
PedenkoIllia <pedenkoia@gmail.com>
2020-08-23 16:38:26 +0300

    Add automapper correct use

---
+++ LogicLayer/Mapper/MapProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using DataAccessLayer.Entities;
+using LogicLayer.DataTransferObjects;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LogicLayer.Mapper
+{
+    public class MapProfile : Profile
+    {
+        public MapProfile()
+        {
+            CreateMap<Record, RecordDTO>();
+            CreateMap<RecordDTO, Record>();
+            CreateMap<IEnumerable<Record>, List<RecordDTO>>();
+        }
+    }
+}
--- LogicLayer/Services/RecordService.cs
+++ LogicLayer/Services/RecordService.cs
@@ -10,41 +10,40 @@ namespace LogicLayer.Services
     public class RecordService : IRecordService
     {
         IUnitOfWork Database { get; set; }
+        private readonly IMapper _mapper;
 
-        public RecordService(IUnitOfWork db)
+
+        public RecordService(IUnitOfWork db, IMapper mapper)
         {
             Database = db;
+            _mapper = mapper;
+        }
+
+        public RecordDTO GetRecord(int id)
+        {
+            Record rec = Database.Records.Get(id);
+            if (rec == null)
+                return null;
+
+            return _mapper.Map<RecordDTO>(rec);
         }
 
         public void AddRecord(RecordDTO rec)
         {
-            Record record = CreateFromDTO(rec);
-
+            Record record = _mapper.Map<Record>(rec);
             Database.Records.Create(record);
             Database.Save();
         }
 
         public void ChangeRecord(RecordDTO rec)
         {
-            Record record = CreateFromDTO(rec);
+            Record record = _mapper.Map<Record>(rec);
             Database.Records.Update(record);
         }
 
         public IEnumerable<RecordDTO> GetRecords()
         {
-            var mapper = new MapperConfiguration(cfg => cfg.CreateMap<Record, RecordDTO>()).CreateMapper();
-            return mapper.Map<IEnumerable<Record>, List<RecordDTO>>(Database.Records.GetAll());
-        }
-
-        private Record CreateFromDTO(RecordDTO rec) 
-        {
-            Record record = new Record()
-            {
-                Code = rec.Code,
-                Name = rec.Name
-            };
-
-            return record;
+            return _mapper.Map<List<RecordDTO>>(Database.Records.GetAll());
         }
 
         public void Dispose()
