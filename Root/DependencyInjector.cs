d3214cbc3540c7b12e4d619874829250d817260a
PedenkoIllia <pedenkoia@gmail.com>
2020-08-23 16:40:12 +0300

    Add Root level for dependency injection

---
+++ Root/DependencyInjector.cs
@@ -0,0 +1,23 @@
+﻿using DataAccessLayer.EntityFramework;
+using DataAccessLayer.Interfaces;
+using DataAccessLayer.Repositories;
+using LogicLayer.Interfaces;
+using LogicLayer.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Root
+{
+    public class DependencyInjector
+    {
+        public DependencyInjector()
+        { }
+
+        public static void InjectDependencies(IServiceCollection services, string databaseConnectionString)
+        {
+            services.AddScoped<IUnitOfWork, RecordUnitOfWork>();
+            services.AddScoped<IRecordService, RecordService>();
+            services.AddDbContext<RecordContext>(options => options.UseSqlServer(databaseConnectionString));
+        }
+    }
+}
