1edefc2058c505f369064d52fb2d531af992d380
PedenkoIllia <pedenkoia@gmail.com>
2020-08-24 18:29:55 +0300

    Add error

---
+++ LogicLayer/Exceptions/RecordNotFoundException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace LogicLayer.Exceptions
+{
+    public class RecordNotFoundException : Exception
+    {
+        public string Property { get; private set; }
+
+        public RecordNotFoundException(string message, string property) : base(message)
+        {
+            Property = property;
+        }
+    }
+}
