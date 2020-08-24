9471f50d895949a92744979f22dbb5ab9445998b
PedenkoIllia <pedenkoia@gmail.com>
2020-08-24 18:33:15 +0300

    Rename RecordModelValidator to RecordValidator

---
+++ WEB/Validators/RecordValidator.cs
@@ -0,0 +1,19 @@
+﻿using FluentValidation;
+using LogicLayer.DataTransferObjects;
+
+namespace WEB.Validators
+{
+    public class RecordValidator : AbstractValidator<RecordDTO>
+    {
+        public RecordValidator()
+        {
+            RuleFor(rec => rec.Code).Matches("^\\d{3}$").WithMessage("Код должен состоять из трех цифр!");
+            RuleFor(rec => rec.Name).NotEmpty().WithMessage("Поле Имя не может быть пустым");
+
+            RuleSet("CorrectId", () =>
+            {
+                RuleFor(x => x.Id).NotEqual(0).WithMessage("Необходимо указать корректный Id");
+            });
+        }
+    }
+}
