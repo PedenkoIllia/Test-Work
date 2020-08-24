d1ef0eedabf1e51d7a2e351eec4beb70117205cc
PedenkoIllia <pedenkoia@gmail.com>
2020-08-24 18:33:39 +0300

    Filter for Cookie

---
+++ WEB/Filters/AddCookieWithIdFilter.cs
@@ -0,0 +1,26 @@
+﻿using LogicLayer.DataTransferObjects;
+using Microsoft.AspNetCore.Mvc.Filters;
+using System;
+
+namespace WEB.Filters
+{
+    public class AddCookieWithIdAttribute : Attribute, IActionFilter
+    {
+        int Id { get; set; }
+
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+            if(context.ModelState.IsValid)
+                context.HttpContext.Response.Cookies.Append("ChangRecordId", Id.ToString());
+        }
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            if (context.ModelState.IsValid)
+            {
+                if(context.ActionArguments.ContainsKey("record"))
+                    Id = ((RecordDTO) context.ActionArguments["record"]).Id;
+            }
+        }
+    }
+}
