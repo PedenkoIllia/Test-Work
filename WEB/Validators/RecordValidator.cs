d88183a09c239da9ab62a416a447c566b395a9dd
PedenkoIllia <pedenkoia@gmail.com>
2020-08-26 11:18:52 +0300

    Change message for code

--- WEB/Validators/RecordValidator.cs
+++ WEB/Validators/RecordValidator.cs
@@ -7,7 +7,7 @@ namespace WEB.Validators
     {
         public RecordValidator()
         {
-            RuleFor(rec => rec.Code).Matches("^\\d{3}$").WithMessage("Код должен состоять из трех цифр!");
+            RuleFor(rec => rec.Code).Matches("^\\d{3}$").WithMessage("Поле Значение должно состоять из трех цифр!");
             RuleFor(rec => rec.Name).NotEmpty().WithMessage("Поле Имя не может быть пустым");
 
             RuleSet("CorrectId", () =>
